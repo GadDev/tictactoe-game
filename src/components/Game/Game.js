5539cd654d32be2a65bdcc6538e1e13cdd5dcea0
Alex Gadaix <alex.gadaix@gmail.com>
2021-04-15 12:09:34 +0100

    scaffold game component, import style boilerplate

---
+++ src/components/Game/Game.js
@@ -0,0 +1,57 @@
+import './Game.css';
+
+import { Fragment } from 'react';
+
+const Game = () => {
+  return (
+    <Fragment>
+      <div id="game">
+        <h1>Tic Tac Toe</h1>
+        <div id="board">
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+          <button className="cell">
+            <span className="cell-content"></span>
+          </button>
+        </div>
+      </div>
+
+      <div id="modal-overlay">
+        <div id="game-result-modal">
+          <div id="result-container">
+            <div id="winner-container">
+              <span></span>
+            </div>
+          </div>
+          <div id="new-game-container">
+            <button id="new-game-button">Start New Game</button>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Game;
